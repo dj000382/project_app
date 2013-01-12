7daf962d313061a440f3ae7eec35ce2ac3b96972
Durando <jimmydurando06@gmail.com>
2013-01-12 14:16:34 +0100

    ajout password + tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,13 +16,38 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(nom: "Dupont", prenom: "Michel" , labo: "laboExemple") }
+  before { @user = User.new(nom: "Dupont", prenom: "Michel" , labo: "laboExemple" , password: "coucouhibou", password_confirmation: "coucouhibou") }
 
   subject { @user }
 
   it { should respond_to(:nom) }
   it { should respond_to(:prenom) }
 	it { should respond_to(:labo) }
+
+	it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+	it { should be_valid }
+	
+  describe "when name is not present" do
+    before { @user.nom = " " }
+    it { should_not be_valid }
+  end
+
+	  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
 end
 
 
